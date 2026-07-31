8fae384ee4c00350968cc62c5eb1d5d0162d7bd3
Apver <website@vaporesso.com>
2026-07-31 14:24:49 +0800

    fix: xros mini end product name

--- app/components/SeriesProduct/XrosSeries/XrosMiniFresh/Layout.jsx
+++ app/components/SeriesProduct/XrosSeries/XrosMiniFresh/Layout.jsx
@@ -241,7 +241,7 @@ export function XrosMiniFreshLayout({children, onCheckSpecs}) {
     ],
   };
   const EndProductData = {
-    title: 'XROS',
+    title: 'XROS ',
     subtitle: 'MINI',
     imgUrl:
       'https://cdn.shopify.com/s/files/1/0703/9873/8521/files/xros-mini-retro-10_1.webp',
