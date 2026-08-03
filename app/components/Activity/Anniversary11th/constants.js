89d819b57ea7ff3706d4b8b882a6de508a8b88a0
Enjue <1742484687@qq.com>
2026-08-03 20:23:57 +0800

    08/03 20.23 edit

--- app/components/Activity/Anniversary11th/constants.js
+++ app/components/Activity/Anniversary11th/constants.js
@@ -1,6 +1,6 @@
 
 export  const ANNIVERSARYSTORY_IMAGES = Array.from(
   { length: 29 },
-  (_, i) => `https://cdn.shopify.com/s/files/1/0703/9873/8521/files/innovation-x-lifestyle-${i + 1}.webp?v=0.1`
+  (_, i) => `https://cdn.shopify.com/s/files/1/0999/4249/8609/files/anniversary-11th-lifestyle-${i + 1}.webp?v=1785759677`
 );
 export  const ANNIVERSARYSTORY_GROUP_COUNTS = [6, 6, 6, 5, 6];
